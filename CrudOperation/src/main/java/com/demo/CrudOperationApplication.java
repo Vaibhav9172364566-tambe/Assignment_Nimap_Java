6bf85be1b1f7df07a62554498915c13e359bd13c
Vaibhav Tambe <tambevaibhav207@gmail.com>
2024-11-14 21:09:34 +0530

    Create Model of Product and Category

--- CrudOperation/src/main/java/com/demo/CrudOperationApplication.java
+++ CrudOperation/src/main/java/com/demo/CrudOperationApplication.java
@@ -9,6 +9,7 @@ public class CrudOperationApplication {
 	
 
 	public static void main(String[] args) {
+		
 		System.out.println("Start........");
 		SpringApplication.run(CrudOperationApplication.class, args);
 		System.out.println("end...........");
